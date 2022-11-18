a5e16e82a9b55ce56128dfe45f79dab781bb6d62
Dorian Lo_Negro <dorian.lonegro@epfedu.fr>
2022-11-18 16:08:27 +0100

    Création d'un constructeur et de deux methode une méthode permettant de savoir si la case est vide ou non et une methode permettant d'ajputer un jeton

--- src/super_puissance4_lo_negro/CelluleDeGrille.java
+++ src/super_puissance4_lo_negro/CelluleDeGrille.java
@@ -10,8 +10,36 @@ package super_puissance4_lo_negro;
  */
 public class CelluleDeGrille {
     //création des différents attributs qui doivent être présenr dans cette classe
-    private Jetons jetonCourant = null; 
-    private boolean AvoirTrouNoir;
-    private boolean avoirDesintegrateur;
+    private Jetons jetonCourant ; 
+    private boolean AvoirTrouNoir ;
+    private boolean avoirDesintegrateur ;
+
+    public CelluleDeGrille(Jetons jetonCourant, boolean AvoirTrouNoir, boolean avoirDesintegrateur) {   //création d'un constructeur 
+        this.jetonCourant = null;// initialement les cases du jeu ne comportent rien
+        this.AvoirTrouNoir = false;
+        this.avoirDesintegrateur = false;
+    }
+    
+    public boolean presenceJeton(){   // création d'une méthode permettant de savoir si la case est vide ou non 
+        if (jetonCourant==null){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    public void setaffecterJeton(Jetons jetonCourant) { // méthode permettant de ajouter le jeton en paramètre à la cellule
+        this.jetonCourant = jetonCourant;
+    }
+
+    
+
+    
+
+    
+
+    
+    
+    
     
 }
