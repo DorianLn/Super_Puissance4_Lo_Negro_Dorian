20e92db8524af3d48f1295a6efbbb03e71bf7fd0
Dorian Lo_Negro <dorian.lonegro@epfedu.fr>
2022-11-18 17:00:35 +0100

    Création de la du constructeur dans PlateauDeJeu

--- src/super_puissance4_lo_negro/PlateauDeJeu.java
+++ src/super_puissance4_lo_negro/PlateauDeJeu.java
@@ -9,5 +9,23 @@ package super_puissance4_lo_negro;
  * @author doria
  */
 public class PlateauDeJeu {
+    CelluleDeGrille grille[][] = new  CelluleDeGrille[6][7];
+
+    public PlateauDeJeu(){ //constructeur pour mettre les cellules du plateau à null
+            
+           for (int i=0; i<5 ; i++){
+               for(int j=0 ; i<6 ; i++){
+                   grille [i][j]=null;
+               }
+           }
+    }
+
+    
+    
+    
+    
+    
+    
+    
     
 }
